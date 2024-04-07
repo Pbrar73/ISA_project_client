6898af3a9f3a83c58375fce9de12c3b88fe09928
Jaskaran Toor <jaskarantoor00@gmail.com>
2024-04-06 17:06:27 -0700

    updatde with new JWT addistions

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const serverBaseUrl = 'https://milestone1server-4a2e0b56cbf7.herokuapp.com';
-
+  
     const registerForm = document.getElementById('register-form');
     if (registerForm) {
         registerForm.addEventListener('submit', function(event) {
             event.preventDefault();
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
-
+  
             fetch(`${serverBaseUrl}/register`, {
                 method: 'POST',
                 headers: {
@@ -26,60 +26,60 @@ document.addEventListener('DOMContentLoaded', function() {
             .catch(error => alert('Error during registration: ' + error));
         });
     }
-
-    // Handling login form submission
-    const loginForm = document.getElementById('login-form');
-    if (loginForm) {
-        loginForm.addEventListener('submit', function(event) {
-            event.preventDefault();
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
-
-            fetch(`${serverBaseUrl}/index`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password }),
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    // Store the JWT token in local storage
-                    localStorage.setItem('token', data.token);
-                    window.location.href = 'protected.html'; 
-                } else {
-                    alert('Login failed: ' + data.message);
-                }
-            })
-            .catch(error => alert('Error during login: ' + error));
-        });
-    }
+  
+   // Handling login form submission
+   const loginForm = document.getElementById('login-form');
+   if (loginForm) {
+       loginForm.addEventListener('submit', function(event) {
+           event.preventDefault();
+           const email = document.getElementById('email').value;
+           const password = document.getElementById('password').value;
+     
+           fetch(`${serverBaseUrl}/login`, {
+               method: 'POST',
+               headers: {
+                   'Content-Type': 'application/json',
+               },
+               body: JSON.stringify({ email, password }),
+           })
+           .then(response => response.json())
+           .then(data => {
+               if (data.success) {
+                   // Store the JWT token received from the server
+                   sessionStorage.setItem('token', data.token);
+                   sessionStorage.setItem('userEmail', email);
+                   window.location.href = 'protected.html'; 
+               } else {
+                   alert('Login failed: ' + data.message);
+               }
+           })
+           .catch(error => alert('Error during login: ' + error));
+       });
+   }
 
     const isProtectedPage = window.location.pathname.includes('protected');
     if (isProtectedPage) {
-        // Retrieve the JWT token from local storage
-        const token = localStorage.getItem('token');
+        const token = sessionStorage.getItem('token');
         if (!token) {
             window.location.href = '/index.html';
-        }
-
-        // Send the JWT token in the request header
-        fetch(`${serverBaseUrl}/check-session`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (!data.success) {
+        } else {
+            fetch(`${serverBaseUrl}/check-session`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                credentials: 'include'
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (!data.success) {
+                    window.location.href = '/index.html';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
                 window.location.href = '/index.html';
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            window.location.href = '/index.html';
-        });
+            });
+        }
     }
 });
